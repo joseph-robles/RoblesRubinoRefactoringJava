5aa1e61529ce58464aa2751e091f87299332d569
joseph-robles <75534573+joseph-robles@users.noreply.github.com>
2023-07-13 00:57:36 -0500

    Subir código con Code Smells refactorizados
    Los Code Smells que prioricé fueron refactorizados exitosamente, pues no volvieron a ser detectados como Code Smells al ser analizados con SonarQube.

--- src/main/java/RentalInfo.java
+++ src/main/java/RentalInfo.java
@@ -1,11 +1,11 @@
 import java.util.HashMap;
 
 public class RentalInfo {
-
+  private static final String REGULAR = "regular";
   public String statement(Customer customer) {
     HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
+    movies.put("F001", new Movie("You've Got Mail", REGULAR));
+    movies.put("F002", new Movie("Matrix", REGULAR));
     movies.put("F003", new Movie("Cars", "childrens"));
     movies.put("F004", new Movie("Fast & Furious X", "new"));
 
@@ -13,24 +13,31 @@ public class RentalInfo {
     int frequentEnterPoints = 0;
     String result = "Rental Record for " + customer.getName() + "\n";
     for (MovieRental r : customer.getRentals()) {
-      double thisAmount = 0;
-
-      // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
-        thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
+        double thisAmount = 0;
+        
+        // determine amount for each movie
+        switch (movies.get(r.getMovieId()).getCode()) {
+            case REGULAR:
+                thisAmount = 2;
+                if (r.getDays() > 2) {
+                    thisAmount += (r.getDays() - 2) * 1.5;
+                }
+                break;
+            case "new":
+                thisAmount = r.getDays() * 3;
+                if (r.getDays() > 2) {
+                    frequentEnterPoints++;
+                }
+                break;
+            case "childrens":
+                thisAmount = 1.5;
+                if (r.getDays() > 3) {
+                    thisAmount += (r.getDays() - 3) * 1.5;
+                }
+                break;
+            default:
+                break;
         }
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
-        thisAmount = r.getDays() * 3;
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
-        thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-        }
-      }
 
       //add frequent bonus points
       frequentEnterPoints++;
